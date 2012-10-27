0f2c65c9216f591b4c6a5fb4f35385f389e4fb1c
Michael Nguyen <michael@Michaels-MacBook.local>
2012-10-27 13:32:22 -0700

    Initial Commit

---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Michael Nguyen on 2012-10-27.
+//  Copyright (c) 2012 Michael Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
---
+++ Calculator/CalculatorViewController.m
@@ -0,0 +1,29 @@
+//
+//  CalculatorViewController.m
+//  Calculator
+//
+//  Created by Michael Nguyen on 2012-10-27.
+//  Copyright (c) 2012 Michael Nguyen. All rights reserved.
+//
+
+#import "CalculatorViewController.h"
+
+@interface CalculatorViewController ()
+
+@end
+
+@implementation CalculatorViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
